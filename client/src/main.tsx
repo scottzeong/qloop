94d5cb08427ba8f0484a3957ee2c8a5ac5cfb608
Scott <scottzeong@gmail.com>
2026-06-16 11:13:51 +0900

    test: remove superjson transformer to isolate transform error

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,6 @@ import { UNAUTHED_ERR_MSG } from '@shared/const';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { httpBatchLink, TRPCClientError } from "@trpc/client";
 import { createRoot } from "react-dom/client";
-import superjson from "superjson";
 import App from "./App";
 import { getLoginUrl } from "./const";
 import "./index.css";
@@ -41,7 +40,6 @@ const trpcClient = trpc.createClient({
   links: [
     httpBatchLink({
       url: "/api/trpc",
-      transformer: superjson,
       fetch(input, init) {
         return globalThis.fetch(input, {
           ...(init ?? {}),
